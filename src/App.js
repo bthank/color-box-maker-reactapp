3bc068da5fe66dcb35016aa4ce5337ec44fa192f
bthank <bthankachan@yahoo.com>
2021-12-18 22:05:08 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import BoxList from './components/BoxList';
+//import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <BoxList />
+      </div>
+    );
+
+  };
+}
+
+export default App;
